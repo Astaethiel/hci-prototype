50fe7b69e8569d9bd469c32e05572e85d50639d5
Astaethiel <marciniak.miko@gmail.com>
2022-02-27 13:15:37 +0000

    Added initial chat application

---
+++ index-old01.js
@@ -0,0 +1,13 @@
+// see https://socket.io/get-started/chat
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
